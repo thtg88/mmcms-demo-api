2337ebe07a74ec3468bae2be7112ad7ae460d258
Marco Marassi <thtg88@users.noreply.github.com>
2019-03-03 15:07:11 +0000

    Re-added created_at as User fillable attribute

--- app/User.php
+++ app/User.php
@@ -16,7 +16,10 @@ class User extends \Thtg88\MmCms\Models\User
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'created_at',
+        'email',
+        'name',
+        'password',
     ];
 
     /**
@@ -25,7 +28,8 @@ class User extends \Thtg88\MmCms\Models\User
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
