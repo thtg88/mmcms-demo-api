44ec77ebbb13f69b25cad7ebf15387c6f1190a34
Marco Marassi <thtg88@users.noreply.github.com>
2019-03-23 16:14:15 +0000

    Added authorize.developer and authorize.administrator middlewares in Http Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,8 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'authorize.administrator' => \Thtg88\MmCms\Http\Middleware\AuthorizeAdministrator::class,
+        'authorize.developer' => \Thtg88\MmCms\Http\Middleware\AuthorizeDeveloper::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
