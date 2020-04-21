f86be1b1f2f776602acc0aebe0f14978b15da176
Marco Marassi <thtg88@users.noreply.github.com>
2020-04-21 21:23:26 +0100

    Remove events listened

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,19 +6,6 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-use Thtg88\MmCms\Events\ContentFieldDestroyed;
-use Thtg88\MmCms\Events\ContentFieldStored;
-use Thtg88\MmCms\Events\ContentModelStored;
-use Thtg88\MmCms\Listeners\MakeContentFieldDropColumnMigration;
-use Thtg88\MmCms\Listeners\MakeContentFieldMigration;
-use Thtg88\MmCms\Listeners\MakeContentModelMigration;
-use Thtg88\MmCms\Listeners\MakeContentModelModel;
-use Thtg88\MmCms\Listeners\MakeContentModelRepository;
-use Thtg88\MmCms\Listeners\MakeContentModelDestroyRequest;
-use Thtg88\MmCms\Listeners\MakeContentModelStoreRequest;
-use Thtg88\MmCms\Listeners\MakeContentModelUpdateRequest;
-use Thtg88\MmCms\Listeners\MakeContentModelController;
-use Thtg88\MmCms\Listeners\DatabaseMigrate;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -30,24 +17,6 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ],
-        ContentModelStored::class => [
-            MakeContentModelMigration::class,
-            MakeContentModelModel::class,
-            MakeContentModelRepository::class,
-            MakeContentModelDestroyRequest::class,
-            MakeContentModelStoreRequest::class,
-            MakeContentModelUpdateRequest::class,
-            MakeContentModelController::class,
-            DatabaseMigrate::class,
-        ],
-        ContentFieldDestroyed::class => [
-            MakeContentFieldDropColumnMigration::class,
-            DatabaseMigrate::class,
-        ],
-        ContentFieldStored::class => [
-            MakeContentFieldMigration::class,
-            DatabaseMigrate::class,
         ],
     ];
 
