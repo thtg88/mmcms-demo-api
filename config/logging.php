27ce9e9102a42d810488bdf9251c93aed6a0098c
Marco Marassi <thtg88@users.noreply.github.com>
2020-05-10 17:09:53 +0100

    Removed stderr logging channel temporarily

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily', 'sentry', 'stderr'],
+            'channels' => ['daily', 'sentry'],
             'ignore_exceptions' => false,
         ],
 
