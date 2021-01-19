37fc129f450b43f4f17a831ae268e8cb7daa6b13
Marco <thtg88@users.noreply.github.com>
2021-01-19 20:47:57 +0000

    Apply fixes from StyleCI

--- app/User.php
+++ app/User.php
@@ -2,14 +2,14 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends \Thtg88\MmCms\Models\User
 {
-    use \Laravel\Passport\HasApiTokens, HasFactory, Notifiable;
+    use \Laravel\Passport\HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
